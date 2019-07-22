a4ded5e9f9812e533c5692d71962085ed935c061
BuildTools <unconfigured@null.spigotmc.org>
2019-07-22 11:59:30 -0700

    this code is for saying hello world

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,39 @@
+import sys,pygame
+from pygame.locals import*
+pygame.init()
+
+
+screen_info = pygame.display.Info()
+x = screen_info.current_w
+y = screen_info.current_h
+
+screen = pygame.display.set_mode((x,y))
+clock = pygame.time.Clock()
+color = (0,255,255)
+
+image = pygame.image.load("fish1.png")
+rect = image.get_rect()
+rect.center = (x//2,y//2)
+
+
+speed = pygame.math.Vector2(0,10)
+def update():
+    rect.move_ip(speed)
+
 def main():
     print("hello world")
+    while True:
+        clock.tick(60)
+        screen.fill(color)
+        update()
+        screen.blit(image, rect)
+        pygame.display.flip()
+
 if __name__ == "__main__":
     main()
 
-
-
-
+import sys,program,random
+random,randomInt(0,360)
 
 
 
